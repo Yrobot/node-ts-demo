8ee015639326361a3e98728d99cde09ae903c75f
Yrobot <Yrobot@qq.com>
2022-07-13 21:08:17 +0800

    update react-dom demo

--- src/reactDom.tsx
+++ src/reactDom.tsx
@@ -1,7 +1,18 @@
 import * as ReactDOMServer from "react-dom/server";
 import React from "react";
 
-const Component = require("./components/Footer").default;
-const html = ReactDOMServer.renderToString(<Component />);
+const Header = () => <header className="header">Header</header>;
+const Content = () => <section className="content">Content</section>;
+const Footer = () => <footer className="footer">Footer</footer>;
+const Page = () => (
+  <div className="page">
+    <Header />
+    <Content />
+    <Footer />
+  </div>
+);
+
+// const Page = require("./components/Footer").default;
+const html = ReactDOMServer.renderToString(<Page />);
 
 export default html;
